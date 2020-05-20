efe8938692c0c9f2cf13ab023359f4f653ddd281
曹勇 <caoyong@tiza.com.cn>
2020-05-20 21:38:02 +0800

    初次提交

---
+++ src/main/java/com/yong/leetcode/yong_leetcode/App.java
@@ -0,0 +1,13 @@
+package com.yong.leetcode.yong_leetcode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
